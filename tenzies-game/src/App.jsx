33c99974b442c6855c74229374aebb10f016e403
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-03-21 22:25:26 +0530

    Added all Dice to the App

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,8 +1,21 @@
-
+import Dice from './Dice'
 function App() {
 
   return(
-      <main className="App">
+      <main className="app">
+          <div className="container">
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+              <Dice />
+
+          </div>
 
       </main>
   )
---
+++ tenzies-game/src/Dice.jsx
@@ -0,0 +1,7 @@
+export default function Dice(){
+   return (
+       <button className="diceButton">
+            1
+       </button>
+   )
+}
