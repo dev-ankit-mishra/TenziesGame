f1b7ff2722a19d01d4444d6a085d56c6c0f76c15
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-03-22 23:21:00 +0530

    Completed Game development

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,57 +1,68 @@
-import {useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import Dice from './Dice'
-import  {nanoid} from 'nanoid'
+import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
+
+
 function App() {
 
-  const [dice, setDice] = useState(()=>generateDice())
+    const [dice, setDice] = useState(() => generateDice())
 
-  const gameWon = dice.every((die)=>die.isHeld)
-                        && dice.every((die)=>die.value===dice[0].value)
-  function generateDice() {
-      return new Array(10)
-                            .fill(0)
-                            .map(()=>({
-                                value : Math.ceil(Math.random()*6),
-                                isHeld :  false,
-                                id : nanoid()
-                            }))
-  }
+    const gameWon = dice.every((die) => die.isHeld) && dice.every((die) => die.value === dice[0].value)
 
-  function rollDice(){
-        if(!gameWon){
-            setDice(oldDice=>oldDice.map((prev)=>
-                prev.isHeld ? prev : {...prev,value : Math.ceil(Math.random()*6)}
-            ))
+    const buttonRef=useRef(null)
+
+    const [count, setCount] = useState(0)
+
+
+    useEffect(() => {
+        if(gameWon){
+            buttonRef.current.focus();
         }
-        else{
+    },[gameWon])
+
+    function generateDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid()
+            }))
+    }
+
+    function rollDice() {
+        if (!gameWon) {
+            setCount(prevCount => prevCount + 1)
+            setDice(oldDice => oldDice.map((prev) => prev.isHeld ? prev : {
+                ...prev,
+                value: Math.ceil(Math.random() * 6)
+            }))
+        } else {
+            setCount(0)
             setDice(generateDice())
         }
 
-  }
+    }
 
-  function hold(id){
-      setDice(dice.map((item)=>(
-          item.id===id ? {...item,isHeld: !item.isHeld} : item
-      )))
-  }
+    function hold(id) {
+        setDice(dice.map((item) => (item.id === id ? {...item, isHeld: !item.isHeld} : item)))
+    }
 
-  const diceButtons = dice.map(button=>(
-      <Dice key={button.id}
-            value={button.value}
-            isHeld={button.isHeld}
-            id={button.id}
-            hold={()=>hold(button.id)}    />
-  ))
-  return(
-      <main className="app">
-          <h1 className="heading">Tenzies</h1>
-          <p className="para">Roll until dice are the same.Click each dice to freeze it at its current value between rolls.</p>
-          <div className="container">
-              {diceButtons}
-          </div>
-          <button onClick={rollDice} className="rollButton">{gameWon ?"New Game" :"Roll"}</button>
-      </main>
-  )
+    const diceButtons = dice.map(button => (<Dice key={button.id}
+                                                  value={button.value}
+                                                  isHeld={button.isHeld}
+                                                  id={button.id}
+                                                  hold={() => hold(button.id)}/>))
+    return (<main className="app">
+            {gameWon && <Confetti />}
+            <h1 className="heading">Tenzies</h1>
+            <p className="para">Roll until dice are the same.Click each dice to freeze it at its current value between
+                rolls.</p>
+            <div className="container">
+                {diceButtons}
+            </div>
+            <button ref={buttonRef} onClick={rollDice} className="rollButton">{gameWon ? "New Game" : "Roll"}</button>
+        {gameWon && <p className="count-para">You took {count} attempts. </p>}
+        </main>)
 }
 
 export default App
--- tenzies-game/src/Dice.jsx
+++ tenzies-game/src/Dice.jsx
@@ -1,7 +1,7 @@
 export default function Dice(props){
 
    return (
-       <button style={props.isHeld ? {backgroundColor : "green"} : {backgroundColor: "white"}} onClick={props.hold} className="diceButton">
+       <button style={props.isHeld ? {backgroundColor : "#00CED1"} : {backgroundColor: "white"}} onClick={props.hold} className="diceButton">
            {props.value}
        </button>
    )
